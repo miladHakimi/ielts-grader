261297974dc6a5ff7c9085069e8e77dd29c3da0e
Milad Hakimi <miladhakimi@Milads-MacBook-Air-3.local>
2023-02-05 20:06:54 -0800

    Add a new interface to robot.
    Rewrite command is now added to the robot.

---
+++ chatgpt.py
@@ -0,0 +1,22 @@
+import os
+
+import openai
+
+
+# ChatGPT Interface
+class ChatGPT:
+    def __init__(self):
+        openai.api_key = os.environ.get('OPENAI_API_KEY')
+    
+    # Puts the requests to the "to be processed" queue.
+    def enqueue(self, req):
+        pass
+    
+    # Reads a request from the queue and returns it.
+    def dequeue(self):
+        pass
+
+    # Sends the request to the ChatGPT server and returns the response.
+    def prompt(self, req):
+        response = openai.Completion.create(model="text-davinci-003", prompt=req, temperature=0.9, max_tokens=1000)
+        return response
