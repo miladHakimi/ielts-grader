660f811f0b7781566f0163abd9ef91ac933e6b4f
Milad Hakimi <miladhakimi33@gmail.com>
2023-07-16 00:11:25 -0700

    Fix speaking bug.

--- src/controllers/speaking.py
+++ src/controllers/speaking.py
@@ -33,6 +33,40 @@ def grade_speaking(message, bot, gpt_api):
     increment_requests(message)
 
 
+def upload_file(file_name):
+    bucket = storage_client.get_bucket(bucket_or_name=BUCKET_NAME)
+    blob = bucket.blob(file_name)
+    blob.upload_from_filename(file_name)
+
+
+def erase_file(file_name):
+    """ Erases file from the local storage and the cloud """
+    bucket = storage_client.get_bucket(bucket_or_name=BUCKET_NAME)
+    blob = bucket.blob(file_name)
+    blob.delete()
+    os.remove(file_name)
+
+
+def get_transcript(file_name):
+    media_info = MediaInfo.parse(file_name)
+    sample_rate = media_info.audio_tracks[0].sampling_rate
+    config = speech.RecognitionConfig(
+        encoding=speech.RecognitionConfig.AudioEncoding.OGG_OPUS,
+        sample_rate_hertz=sample_rate,
+        enable_automatic_punctuation=True,
+        language_code="en-US",
+    )
+    audio = speech.RecognitionAudio(
+        uri='gs://%s/%s' %
+        (BUCKET_NAME, file_name))
+    response = client.long_running_recognize(
+        config=config, audio=audio).result(
+        timeout=500)
+    transcript = "".join(
+        [result.alternatives[0].transcript for result in response.results])
+    return transcript
+
+
 def process_speaking_step(message, bot, gpt_api):
     if message.voice:
         bot.reply_to(message, "Please wait a couple of minutes while we process your voice.")
@@ -47,30 +81,17 @@ def process_speaking_step(message, bot, gpt_api):
         file_name_ogg = "{}_{}.ogg".format(user_id, timestamp)
         with open(file_name_ogg, 'wb') as new_file:
             new_file.write(downloaded_file)
-        media_info = MediaInfo.parse(file_name_ogg)
-        sample_rate = media_info.audio_tracks[0].sampling_rate
-        encoding = speech.RecognitionConfig.AudioEncoding.OGG_OPUS
+        try:
+            upload_file(file_name_ogg)
+            transcript = get_transcript(file_name_ogg)
+            bot.send_chat_action(chat_id=message.chat.id, action="typing")
+        except Exception as e:
+            bot.reply_to(message, "An error has occured while processing your input. Please try again.")
+            print(e)
+            erase_file(file_name_ogg)
+            return
+        erase_file(file_name_ogg)
 
-        config = speech.RecognitionConfig(
-            encoding=encoding,
-            sample_rate_hertz=sample_rate,
-            enable_automatic_punctuation=True,
-            language_code="en-US",
-        )
-        bucket = storage_client.get_bucket(bucket_or_name=BUCKET_NAME)
-
-        blob = bucket.blob(file_name_ogg)
-        blob.upload_from_filename(file_name_ogg)
-        audio = speech.RecognitionAudio(
-            uri='gs://%s/%s' %
-            (BUCKET_NAME, file_name_ogg))
-        response = client.long_running_recognize(
-            config=config, audio=audio).result(
-            timeout=500)
-        transcript = "".join(
-            [result.alternatives[0].transcript for result in response.results])
-        blob.delete()
-        bot.send_chat_action(chat_id=message.chat.id, action="typing")
         marking_prompt = "Suppose you're an IELTS speaking reviewer. Qualitatively assess my ielts speaking task based on these factors: " \
             "Coherence and flow:\n"\
             "Sentence structure and syntax:\n"\
@@ -78,14 +99,13 @@ def process_speaking_step(message, bot, gpt_api):
             "Lack of hesitations and repetitions: \n"\
             "Finally, give it a grade range base on academic ielts grading system form 0 to 9. "\
             "The min and max in the grade range must be 1 point apart. State exactly what is wrong and how to fix it. Start with \"Coherence and flow:\".\n{}".format(transcript)
+
         response = gpt_api.prompt(marking_prompt)
         bot.reply_to(
             message,
             "Recognized text: {}\n\nGrade:{}".format(
                 transcript,
                 response.choices[0].text))
-        # Remove files
-        os.remove(file_name_ogg)
 
 
 def generate_idea(message, bot, gpt_api):
